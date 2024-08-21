1a5f5d70198c85c16cf2874fc7b5eadc60d78ee1
FloatingComet62 <shubh2007rai@gmail.com>
2024-08-21 11:14:19 +0530

    bitboards, boards and stuff

--- src/board.zig
+++ src/board.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const Piece = enum(u4) {
     W_KING,
     W_QUEEN,
@@ -16,24 +18,91 @@ pub const Piece = enum(u4) {
     B_PAWN,
     B_PASSANTABLE_PAWN,
 };
+pub const PiecePresentable = enum(u4) {
+    W_KING,
+    W_QUEEN,
+    W_ROOK,
+    W_BISHOP,
+    W_KNIGHT,
+    W_PAWN,
+    B_KING,
+    B_QUEEN,
+    B_ROOK,
+    B_BISHOP,
+    B_KNIGHT,
+    B_PAWN,
+};
 
-pub fn starting_position() [64]?Piece {
-    const board = [64]?Piece{
-        .W_CASTLEABLE_ROOK, .W_KNIGHT, .W_BISHOP, .W_QUEEN, .W_KING, .W_BISHOP, .W_KNIGHT, .W_CASTLEABLE_ROOK,
-        .W_PAWN,            .W_PAWN,   .W_PAWN,   .W_PAWN,  .W_PAWN, .W_PAWN,   .W_PAWN,   .W_PAWN,
-        null,               null,      null,      null,     null,    null,      null,      null,
-        null,               null,      null,      null,     null,    null,      null,      null,
-        null,               null,      null,      null,     null,    null,      null,      null,
-        null,               null,      null,      null,     null,    null,      null,      null,
-        .B_PAWN,            .B_PAWN,   .B_PAWN,   .B_PAWN,  .B_PAWN, .B_PAWN,   .B_PAWN,   .B_PAWN,
-        .B_CASTLEABLE_ROOK, .B_KNIGHT, .B_BISHOP, .B_QUEEN, .B_KING, .B_BISHOP, .B_KNIGHT, .B_CASTLEABLE_ROOK,
-    };
-    return board;
-}
+pub const Board = struct {
+    const Self = @This();
+
+    pieces: [64]?Piece,
+    pub fn starting_position() Self {
+        return .{
+            .pieces = .{
+                .W_CASTLEABLE_ROOK, .W_KNIGHT, .W_BISHOP, .W_QUEEN, .W_KING, .W_BISHOP, .W_KNIGHT, .W_CASTLEABLE_ROOK,
+                .W_PAWN,            .W_PAWN,   .W_PAWN,   .W_PAWN,  .W_PAWN, .W_PAWN,   .W_PAWN,   .W_PAWN,
+                null,               null,      null,      null,     null,    null,      null,      null,
+                null,               null,      null,      null,     null,    null,      null,      null,
+                null,               null,      null,      null,     null,    null,      null,      null,
+                null,               null,      null,      null,     null,    null,      null,      null,
+                .B_PAWN,            .B_PAWN,   .B_PAWN,   .B_PAWN,  .B_PAWN, .B_PAWN,   .B_PAWN,   .B_PAWN,
+                .B_CASTLEABLE_ROOK, .B_KNIGHT, .B_BISHOP, .B_QUEEN, .B_KING, .B_BISHOP, .B_KNIGHT, .B_CASTLEABLE_ROOK,
+            },
+        };
+    }
+    pub fn null_position() Self {
+        return .{
+            .pieces = .{
+                null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null,
+            },
+        };
+    }
+    pub fn get(self: Self, file: File, rank: Rank) ?Piece {
+        return self.pieces[square(file, rank)];
+    }
+    pub fn get_piece_positions(self: Self, allocator: std.mem.Allocator, piece: Piece, count: ?u6) ![]u6 {
+        const actual_count = count orelse 10;
+        var squares = try std.ArrayList(u6).initCapacity(allocator, actual_count);
+        for (0.., self.pieces) |i, board_pieces| {
+            if (piece != board_pieces) {
+                continue;
+            }
+            squares.appendAssumeCapacity(@intCast(i));
+            if (squares.items.len == actual_count) {
+                break;
+            }
+        }
+        return squares.items;
+    }
+    pub fn generate_raw_bitboards(self: Self) [16]u64 {
+        var bitboards = [16]u64{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        for (0.., self.pieces) |i, maybe_piece| {
+            const piece = maybe_piece orelse continue;
+            bitboards[@intFromEnum(piece)] |= @as(u64, 1) << @intCast(i);
+        }
+        return bitboards;
+    }
+    pub fn generate_bitboards(self: Self) *[12]u64 {
+        var bitboards = self.generate_raw_bitboards();
+        return bitboards[0..2] ++ .{bitboards[2] | bitboards[3]} // White Rooks
+        ++ bitboards[4..6] ++ .{bitboards[6] | bitboards[7]} // White Pawns
+        ++ bitboards[8..10] ++ .{bitboards[10] | bitboards[11]} // Black Rooks
+        ++ bitboards[12..14] ++ .{bitboards[14] | bitboards[15]} // Black Pawns
+        ;
+    }
+};
 
 pub const File = enum(u3) { A, B, C, D, E, F, G, H };
 pub const Rank = enum(u3) { _1, _2, _3, _4, _5, _6, _7, _8 };
 
-pub fn square(file: File, rank: Rank) u7 {
-    return @intFromEnum(file) + @as(u7, @intFromEnum(rank)) * 8;
+pub fn square(file: File, rank: Rank) u6 {
+    return @intFromEnum(file) + @as(u6, @intFromEnum(rank)) * 8;
 }
--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 const testing = std.testing;
+const BoardData = @import("board.zig");
+const Piece = BoardData.Piece;
+const PiecePresentable = BoardData.PiecePresentable;
+const Board = BoardData.Board;
 
 export fn add(a: i32, b: i32) i32 {
     return a + b;
@@ -7,4 +11,52 @@ export fn add(a: i32, b: i32) i32 {
 
 test "basic add functionality" {
     try testing.expect(add(3, 7) == 10);
+}
+
+test "Pieces alignment" {
+    std.debug.print("\nPieces alignment\n", .{});
+    std.debug.print("Piece\t\t\t |Value\n", .{});
+    std.debug.print("-------------------------|-------\n", .{});
+    std.debug.print("White King\t\t |{}\n", .{@intFromEnum(Piece.W_KING)});
+    std.debug.print("White Queen\t\t |{}\n", .{@intFromEnum(Piece.W_QUEEN)});
+    std.debug.print("White Rook\t\t |{}\n", .{@intFromEnum(Piece.W_ROOK)});
+    std.debug.print("White Rook(Castleable)   |{}\n", .{@intFromEnum(Piece.W_CASTLEABLE_ROOK)});
+    std.debug.print("White Bishop\t\t |{}\n", .{@intFromEnum(Piece.W_BISHOP)});
+    std.debug.print("White Knight\t\t |{}\n", .{@intFromEnum(Piece.W_KNIGHT)});
+    std.debug.print("White Pawn\t\t |{}\n", .{@intFromEnum(Piece.W_PAWN)});
+    std.debug.print("White Pawn(Passantable)  |{}\n", .{@intFromEnum(Piece.W_PASSANTABLE_PAWN)});
+    std.debug.print("Black King\t\t |{}\n", .{@intFromEnum(Piece.B_KING)});
+    std.debug.print("Black Queen\t\t |{}\n", .{@intFromEnum(Piece.B_QUEEN)});
+    std.debug.print("Black Rook\t\t |{}\n", .{@intFromEnum(Piece.B_ROOK)});
+    std.debug.print("Black Rook(Castleable)   |{}\n", .{@intFromEnum(Piece.B_CASTLEABLE_ROOK)});
+    std.debug.print("Black Bishop\t\t |{}\n", .{@intFromEnum(Piece.B_BISHOP)});
+    std.debug.print("Black Knight\t\t |{}\n", .{@intFromEnum(Piece.B_KNIGHT)});
+    std.debug.print("Black Pawn\t\t |{}\n", .{@intFromEnum(Piece.B_PAWN)});
+    std.debug.print("Black Pawn(Passantable)  |{}\n", .{@intFromEnum(Piece.B_PASSANTABLE_PAWN)});
+}
+
+test "Starting position" {
+    const board = Board.starting_position();
+    try testing.expect(board.get(.E, ._1).? == .W_KING);
+    try testing.expect(board.get(.B, ._8).? == .B_KNIGHT);
+    try testing.expect(board.get(.F, ._7).? == .B_PAWN);
+    try testing.expect(board.get(.H, ._1).? == .W_CASTLEABLE_ROOK);
+    try testing.expect(board.get(.H, ._8).? == .B_CASTLEABLE_ROOK);
+}
+
+test "Piece Position Pickup" {
+    const board = Board.starting_position();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    try testing.expect((try board.get_piece_positions(arena.allocator(), .W_KING, 1))[0] == 4);
+}
+
+test "Bitboard Generation" {
+    std.debug.print("\nBitboard Generation\n", .{});
+    const board = Board.starting_position();
+    for (0..12) |i| {
+        std.debug.print("{}: {b}\n", .{ @as(PiecePresentable, @enumFromInt(i)), board.generate_bitboards()[i] });
+    }
+    try testing.expect(board.generate_bitboards()[2] == 0b10000001);
+    try testing.expect(board.generate_bitboards()[4] == 0b01000010);
 }
